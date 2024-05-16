b13add1c64ff1a5870b86f023f401bf6cd738fbf
J525252 <128289246+J525252@users.noreply.github.com>
2024-05-17 02:03:17 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,31 +1,25 @@
-def solve():
-    n = int(input())
-    printresult = []
+N=int(input())
+yon=[]
 
-    for i in range(n):
-        s = input()
-        if (s.count('(') != s.count(')')):
-            printresult.append("NO")
-        else:
-            countx = 0
-            county = 0
-            no = 0
+for i in range(N):
+    input_str=input()
+    op=0
+    cl=0
+    a=True
 
-            for j in range(len(s)):
-                if (s[j] == '('):
-                    countx += 1
-                else:
-                    county += 1
+    for char in input_str:
+        if char=='(':
+            op +=1
+        elif char==')':
+            cl +=1
+            if cl>op:
+                a=False
+                break
 
-                if (countx < county):
-                    no = 1
+    if a and op==cl:
+        yon.append('YES')
+    else:
+        yon.append('NO')
 
-            if (no == 1):
-                printresult.append("NO")
-            else:
-                printresult.append("YES")
 
-    print('\n'.join(printresult))
-    
-    
-solve()
+print('\n'.join(yon) )
